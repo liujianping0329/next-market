8a27d2eb2305e9baa2484de3006c0181a2e99041
ljp <136174298@qq.com>
2026-02-09 16:34:06 +0900

    aikey out

--- src/app/api/ai/obtain/route.js
+++ src/app/api/ai/obtain/route.js
@@ -3,12 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function POST(request, context) {
 
-    const ai = new GoogleGenAI({ apiKey: "AIzaSyDmKNV2Jj62OAsLxfCT0kgeJpBL4grgCLQ" });
+    // const ai = new GoogleGenAI({ apiKey: "AIzaSyDmKNV2Jj62OAsLxfCT0kgeJpBL4grgCLQ" });
 
-    const response = await ai.models.generateContent({
-        model: 'gemini-2.5-flash',
-        contents: '生成一篇优美的短文',
-    });
+    // const response = await ai.models.generateContent({
+    //     model: 'gemini-2.5-flash',
+    //     contents: '生成一篇优美的短文',
+    // });
 
-    return NextResponse.json({ ans: response.text });
+    // return NextResponse.json({ ans: response.text });
+    return NextResponse.json({ ans: "这是一个示例回答。" });
 }
