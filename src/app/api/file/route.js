fdcae4554ce7472a7a58c752b8454b8b82f4fb47
ljp <136174298@qq.com>
2026-02-14 17:15:46 +0900

    feat: add image compression for file uploads using browser-image-compression

--- src/app/api/file/route.js
+++ src/app/api/file/route.js
@@ -1,14 +1,19 @@
 import { NextResponse } from "next/server";
-//import supabase from "../../../utils/database";
+import imageCompression from "browser-image-compression";
 import supabase from "@/app/utils/database";
 export async function POST(request, context) {
     console.log("Request received in file upload route");
     const formData = await request.formData();
     const files = formData.getAll("files");
     const urls = [];
+    const ImgOptions = {
+      maxSizeMB: 0.8,        // 最大 0.5MB
+      maxWidthOrHeight: 1200 // 最长边 1200px
+    };
     for (const file of files) {
       const fileName = crypto.randomUUID() + "-" + file.name;
-      await supabase.storage.from("garden").upload("uploads/" + fileName, file);
+      const compressedFile = await imageCompression(file, ImgOptions);
+      await supabase.storage.from("garden").upload("uploads/" + fileName, compressedFile);
       const { data } = supabase.storage.from("garden").getPublicUrl(`uploads/${fileName}`);
 
       urls.push(data.publicUrl);
