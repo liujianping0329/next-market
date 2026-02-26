e12db040833a3ce508c8c9ca501555b062531b93
ljp <136174298@qq.com>
2026-02-26 23:10:29 +0900

    comple err

--- src/app/api/money/harvest/list/route.js
+++ src/app/api/money/harvest/list/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import supabase from "../../../utils/database";
+import supabase from "@/app/utils/database";
 
 export async function GET() {
     const { data: allList } = await supabase.from("harvest").select().order('date', { ascending: false });
