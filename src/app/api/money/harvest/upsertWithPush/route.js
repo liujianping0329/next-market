46c4f4dd1094a90771559113d7bf5eb615df8d16
ljp <136174298@qq.com>
2026-03-06 18:35:30 +0900

    推送加链接

--- src/app/api/money/harvest/upsertWithPush/route.js
+++ src/app/api/money/harvest/upsertWithPush/route.js
@@ -7,8 +7,10 @@ export async function POST(request, context) {
     const { data, error } = await supabase.from('harvest').upsert(requestBody).select();
 
     let pushInfo;
-    console.log(requestBody);
     if (requestBody.userId) {
+        const origin = new URL(request.url).origin;
+        const sendAfter =
+            requestBody.startTime?.replace(" ", "T") + ":00+09:00";
         pushInfo = await ky.post(
             "https://onesignal.com/api/v1/notifications",
             {
@@ -19,7 +21,11 @@ export async function POST(request, context) {
                     app_id: process.env.NEXT_PUBLIC_ONESIGNAL_APPID,
                     include_external_user_ids: [requestBody.userId],
                     contents: { en: requestBody.title },
-                    send_after: requestBody.startTime.replace(" ", "T") + ":00+09:00"
+                    web_url: requestBody.gardenId ? `${origin}/money/garden/greengrass/${requestBody.gardenId}`
+                        : `${origin}/money/garden?tag=harvest`,
+                    ...(new Date(sendAfter) > new Date() && {
+                        send_after: sendAfter
+                    })
                 },
             }
         ).json();
