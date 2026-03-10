e4ae9bd33188cf0be1caddc469975573e5009a2c
liujianping0329 <136174298@qq.com>
2026-03-10 02:37:06 +0000

    推送调试

--- src/app/utils/date.js
+++ src/app/utils/date.js
@@ -80,13 +80,5 @@ export const diffHours = (a, b) => {
 export const calcRemindTime = (startTime, remindBeforeMin) => {
       const d = new Date(startTime);
       d.setMinutes(d.getMinutes() - remindBeforeMin);
-
-      const yyyy = d.getFullYear();
-      const MM = String(d.getMonth() + 1).padStart(2, "0");
-      const dd = String(d.getDate()).padStart(2, "0");
-      const hh = String(d.getHours()).padStart(2, "0");
-      const mm = String(d.getMinutes()).padStart(2, "0");
-      const ss = String(d.getSeconds()).padStart(2, "0");
-
-      return `${yyyy}-${MM}-${dd}T${hh}:${mm}:${ss}+09:00`;
+      return d.toISOString();
 }
