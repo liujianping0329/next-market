9559b2bbe5855f81856b09cf9ef310c6beddec7d
liujianping0329 <136174298@qq.com>
2026-03-17 09:02:08 +0000

    1212

--- src/app/api/granary/upsert/all/route.js
+++ src/app/api/granary/upsert/all/route.js
@@ -6,54 +6,77 @@ import { applyPlanetFilter } from "@/app/utils/query";
 export async function POST(request, context) {
     const { id, userId, date, cash, ...detail } = await request.json();
 
-
-    // const { data: isExistDate } = await supabase.from("granary").select()
-    //     .eq("date", date).eq("label", requestBody.label).maybeSingle();
-    // console.log("isExistLabel", isExistLabel);
-    // if (isExistLabel) {
-    //     return NextResponse.json({
-    //         errorCode: "LABEL_EXISTS",
-    //         errorMsg: "类别名称已存在"
-    //     }, { status: 400 });
-    // }
-
-
-    if (id) {
-        await supabase.from('granary_detail').delete().eq("granaryId", id);
+    let planetId = null;
+    if (userId) {
+        const { data: fuser } = await supabase.from('f_user').select().eq("id", userId).single();
+        planetId = fuser.planetId;
     }
 
-    let query = supabase.from("granary_user_template").select();
-    query = applyPlanetFilter(query, { planetId: null, userId });
+    const { data: granary } = await supabase.from('granary').select().eq("date", date).eq("planetId", planetId).maybeSingle();
 
-    const { data: granaryTemplates } = await query;
-    const cashRateMap = {
-        cny: cash.cnyToJpy / 10000,
-        wjpy: 1,
-        twd: cash.twdToJpy / 10000,
-        usd: cash.usdToJpy / 10000
-    };
+    // if (id) {
+    //     if (userId) {
+    //         await supabase.from('granary_detail').delete().eq("date", date).eq("userId", userId);
+    //     } else {
+    //         await supabase.from('granary_detail').delete().eq("date", date).is("userId", null);
+    //     }
+    // } else {
+    //     if (userId) {
+    //         const { data: detailDataCheck } = await supabase.from("granary_detail").select()
+    //             .eq("date", date).eq("userId", userId);
+    //         if (detailDataCheck.length > 0) {
+    //             return NextResponse.json({
+    //                 errorCode: "DATE_EXISTS",
+    //                 errorMsg: "同一用户每天只能新增一次数据"
+    //             }, { status: 400 });
+    //         }
+    //     } else {
+    //         const { data: detailDataCheck } = await supabase.from("granary_detail").select()
+    //             .eq("date", date).is("userId", null);
+    //         if (detailDataCheck.length > 0) {
+    //             return NextResponse.json({
+    //                 errorCode: "DATE_EXISTS",
+    //                 errorMsg: "匿名用户每天只能新增一次数据"
+    //             }, { status: 400 });
+    //         }
+    //     }
+    // }
 
-    const total = Object.entries(detail).reduce((sum, [value, price]) => {
-        const template = granaryTemplates.find(n => n.value === value);
-        if (!template) return sum;
+    // let query = supabase.from("granary_user_template").select();
+    // query = applyPlanetFilter(query, { planetId: null, userId });
 
-        const rate = cashRateMap[template.cashType] ?? 1;
-        return sum + price * rate;
-    }, 0);
+    // const { data: granaryTemplates } = await query;
+    // const cashRateMap = {
+    //     cny: cash.cnyToJpy / 10000,
+    //     wjpy: 1,
+    //     twd: cash.twdToJpy / 10000,
+    //     usd: cash.usdToJpy / 10000
+    // };
 
-    const { data: granaryUpserted } = await supabase.from('granary').upsert({
-        ...(id && { id }),
-        date, ...(cash || {}),
-        ...(userId && { userId }),
-        total
-    }).select().single();
+    // const total = Object.entries(detail).reduce((sum, [value, price]) => {
+    //     const template = granaryTemplates.find(n => n.value === value);
+    //     if (!template) return sum;
 
-    const detailRows = Object.entries(detail).map(([value, price]) => ({
-        price,
-        templateId: granaryTemplates.find(n => n.value === value)?.id,
-        granaryId: granaryUpserted.id,
-        ...(userId && { userId }),
-    }));
-    const { data: details } = await supabase.from('granary_detail').upsert(detailRows).select();
-    return NextResponse.json({ details });
+    //     const rate = cashRateMap[template.cashType] ?? 1;
+    //     return sum + price * rate;
+    // }, 0);
+
+    // const { data: granaryUpserted } = await supabase.from('granary').upsert({
+    //     ...(id && { id }),
+    //     date, ...(cash || {}),
+    //     ...(userId && { userId }),
+    //     total
+    // }).select().single();
+
+    // const { data: granaryUpserted } = await supabase.from('granary').select().eq().single();
+
+
+
+    // const detailRows = Object.entries(detail).map(([value, price]) => ({
+    //     price,
+    //     templateId: granaryTemplates.find(n => n.value === value)?.id,
+    //     ...(userId && { userId }),
+    // }));
+    // const { data: details } = await supabase.from('granary_detail').upsert(detailRows).select();
+    return NextResponse.json({});
 }
