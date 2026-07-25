099f1dc5daa07838ba6c30b6ac2dc78a245fad77
ljp <136174298@qq.com>
2026-07-25 22:42:29 +0900

    增量更新ai标签

--- src/app/api/garden_cate/list/matchTheGarden/route.js
+++ src/app/api/garden_cate/list/matchTheGarden/route.js
@@ -4,9 +4,15 @@ import supabase from "@/app/utils/database";
 
 export async function POST(request, context) {
     const { prompt, needAll, id } = await request.json();
+    let query = null;
 
-    let query = supabase.from("garden_cate")
-        .select("garden(id,title,content,location,pics,garden_ai(ansJSON),garden_ext(refInfo))").eq("id", id).single();
+    if (needAll) {
+        await supabase.from("garden_labels").delete().eq("cateId", id);
+        query = supabase.from("garden_cate").select("garden(id,title,content,location,pics,garden_ai(ansJSON),garden_ext(refInfo))").eq("id", id).single();
+    } else {
+        query = supabase.from("garden_cate").select(`garden(id,title,content,location,pics,garden_ai(ansJSON),garden_ext(refInfo)
+            ,no_labels:garden_labels!garden_labels_gardenId_fkey())`).eq("id", id).is("garden.no_labels", null).single();
+    }
 
     if (prompt) {
         await supabase.from("constants").update({ value: prompt }).eq("category", "GardenCateExport");
